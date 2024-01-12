db5a5e71cbcc5af0631bea5c7c885ea539c226b4
Lael Barlow <lael@ualberta.ca>
2024-01-11 20:53:10 -0700

    Update type handling

--- tests/test_main.py
+++ tests/test_main.py
@@ -11,7 +11,7 @@ def runner() -> CliRunner:
     return CliRunner()
 
 
-def test_main_succeeds(runner: CliRunner, tmp_path) -> None:
+def test_main_succeeds(runner: CliRunner) -> None:
     """It exits with a status code of zero."""
     result = runner.invoke(
         __main__.main,
