e48ada8bf361e21c48fd9c95a57a8c0f457a62b8
Lael Barlow <lael@ualberta.ca>
2024-01-16 11:27:04 -0700

    Modify and test complex CDS feature parsing

--- src/proksee_batch/genbank_to_cgview_json.py
+++ src/proksee_batch/genbank_to_cgview_json.py
@@ -91,19 +91,161 @@ def genbank_to_cgview_json(genbank_file: str, json_file: str) -> None:
                 else None
             )
 
-            if locations is None:
-                continue  # Or handle the unexpected location type if necessary
+            # Determine strand.
+            strand = 0
+            if {-1} == {loc.strand for loc in locations}:
+                strand = -1
+            elif {1} == {loc.strand for loc in locations}:
+                strand = 1
+            else:
+                raise ValueError(
+                    "Strand is not consistent among locations within feature {}".format(
+                        feature
+                    )
+                )
+
+            # Determine whether any features start or end at the origin.
+            spans_origin = False
+            for loc in locations:
+                if (
+                    loc.start == 0
+                    or loc.start == len(record.seq)
+                    or loc.end == 0
+                    or loc.end == len(record.seq)
+                ):
+                    spans_origin = True
+                    break
 
             for loc in locations:
-                start = int(loc.start) + 1
-                end = int(loc.end)
-                strand = loc.strand
+                start = (
+                    int(loc.start) + 1
+                )  # Biopython converts to 0-based indexing, but CGView uses 1-based indexing (like GenBank format).
+                stop = int(loc.end)
+
+                # Re-order start and end positions if necessary to conform to
+                # the CGView convention.
+                if strand == -1:
+                    start, stop = stop, start
+
+                # If the feature is a CDS and spans the origin (has locations
+                # that start or stop there), then process it accordingly.
+                if feature.type == "CDS" and spans_origin:
+                    if strand == 1:
+                        # Sum the length of all preceding locations.
+                        preceding_length = 0
+                        for preceding_loc_start, preceding_loc_stop in [
+                            (loc.start + 1, loc.end) for loc in locations
+                        ]:
+                            if preceding_loc_stop < start:
+                                preceding_length += (
+                                    abs(preceding_loc_stop - preceding_loc_start) + 1
+                                )
+                        # Determine if the location ends at the origin.
+                        if stop == len(record.seq):
+                            # Determine if the length of the location is a multiple of three.
+                            if (
+                                abs(
+                                    (preceding_length + stop)
+                                    - (preceding_length + start)
+                                )
+                                + 1
+                            ) % 3 != 0:
+                                # Reduce the value of the stop position until the length of the location is a multiple of three.
+                                while (
+                                    abs(
+                                        (preceding_length + stop)
+                                        - (preceding_length + start)
+                                    )
+                                    + 1
+                                ) % 3 != 0:
+                                    stop -= 1
+                        # Determine if the location starts at the origin.
+                        if start == 1:
+                            # Determine if the length of the location is a multiple of three.
+                            if (
+                                abs(
+                                    (preceding_length + stop)
+                                    - (preceding_length + start)
+                                )
+                                + 1
+                            ) % 3 != 0:
+                                # Increase the value of the start position until the length of the location is a multiple of three.
+                                while (
+                                    abs(
+                                        (preceding_length + stop)
+                                        - (preceding_length + start)
+                                    )
+                                    + 1
+                                ) % 3 != 0:
+                                    start += 1
+                    elif strand == -1:
+                        # Sum the length of all preceding locations.
+                        preceding_length = 0
+                        for preceding_loc_start, preceding_loc_stop in [
+                            (loc.end, loc.start + 1) for loc in locations
+                        ]:
+                            if preceding_loc_stop > start:
+                                preceding_length += (
+                                    abs(preceding_loc_stop - preceding_loc_start) + 1
+                                )
+                        # Determine if the location starts at the origin.
+                        if start == len(record.seq):
+                            # Determine if the length of the location is a multiple of three.
+                            if (
+                                abs(
+                                    (preceding_length + stop)
+                                    - (preceding_length + start)
+                                )
+                                + 1
+                            ) % 3 != 0:
+                                # Reduce the value of the start position until the length of the location is a multiple of three.
+                                while (
+                                    abs(
+                                        (preceding_length + stop)
+                                        - (preceding_length + start)
+                                    )
+                                    + 1
+                                ) % 3 != 0:
+                                    start -= 1
+                        # Determine if the location ends at the origin.
+                        if stop == 1:
+                            # Determine if the length of the location is a multiple of three.
+                            if (
+                                abs(
+                                    (preceding_length + stop)
+                                    - (preceding_length + start)
+                                )
+                                + 1
+                            ) % 3 != 0:
+                                # Increase the value of the stop position until the length of the location is a multiple of three.
+                                while (
+                                    abs(
+                                        (preceding_length + stop)
+                                        - (preceding_length + start)
+                                    )
+                                    + 1
+                                ) % 3 != 0:
+                                    stop += 1
+
+                # Assign name based on availability of attributes.
+                name = ""
+                if feature.qualifiers.get("locus_tag") is not None:
+                    if feature.qualifiers.get("locus_tag")[0] != "":
+                        name = feature.qualifiers.get("locus_tag")[0]
+                if name == "":
+                    if feature.qualifiers.get("gene") is not None:
+                        if feature.qualifiers.get("gene")[0] != "":
+                            name = feature.qualifiers.get("gene")[0]
+                if name == "":
+                    if feature.qualifiers.get("product") is not None:
+                        if feature.qualifiers.get("product")[0] != "":
+                            name = feature.qualifiers.get("product")[0]
 
                 feature_data = {
                     "type": feature.type,
-                    "name": feature.qualifiers.get("locus_tag", [""])[0],
+                    "name": name,
                     "start": start,
-                    "stop": end,
+                    "stop": stop,
                     "strand": strand,
                     "source": "genbank-features",
                     "contig": record.name,
--- tests/test_genbank_to_cgview_json.py
+++ tests/test_genbank_to_cgview_json.py
@@ -64,3 +64,123 @@ def test_genbank_to_cgview_json_on_compound_location() -> None:
                 assert (
                     num_cds_features == 2
                 ), "There should be two CDS features in the JSON file, because the CDS feature location is a CompoundLocation with two exons."
+
+
+def test_genbank_to_cgview_on_origin_in_codon() -> None:
+    # Define path to an example genbank file (it is in the tests/data directory).
+    with resources.path("tests.data.genbank", "minimal_example_3.gbk") as genbank_path:
+        genbank_file = str(genbank_path)
+
+        # Define path for a temporary output directory.
+        with tempfile.TemporaryDirectory() as temp_output:
+            # Define path to temporary json file.
+            json_file = os.path.join(temp_output, "temp.json")
+
+            # Use the genbank_to_cgview_json function to convert the genbank file to a CGView JSON file.
+            genbank_to_cgview_json(genbank_file, json_file)
+
+            # Check that the JSON file was created.
+            assert os.path.exists(json_file)
+
+            # Check that the JSON file is not empty.
+            assert os.path.getsize(json_file) > 0
+
+            # Check that there are the correct number of CDS features in the JSON file.
+            with open(json_file) as json_fh:
+                json_data = json.load(json_fh)
+                num_cds_features = 0
+                for feature in json_data["cgview"]["features"]:
+                    if feature["type"] == "CDS":
+                        num_cds_features += 1
+                assert (
+                    num_cds_features == 4
+                ), """There should be four CDS features in the JSON file,
+                because one CDS feature location is split up by the origin."""
+
+            # Check that the length of each coding sequence is a multiple of three.
+            for feature in json_data["cgview"]["features"]:
+                if feature["type"] == "CDS":
+                    assert (
+                        abs(feature["stop"] - feature["start"]) + 1
+                    ) % 3 == 0, """The length of each coding sequence should be a multiple
+                    of three, otherwise the reading frame may be disrupted by
+                    splitting the original coding sequence at the origin."""
+
+            # Check that specific expected coding sequences are present.
+            expected_cds_features = [
+                {
+                    "type": "CDS",
+                    "name": "VHWIEOXL_CDS249",
+                    "start": 3,
+                    "stop": 239,
+                    "strand": 1,
+                    "source": "genbank-features",
+                    "contig": "AB123456",
+                    "legend": "CDS",
+                },
+                {
+                    "type": "CDS",
+                    "name": "VHWIEOXL_CDS249",
+                    "start": 140888,
+                    "stop": 141097,
+                    "strand": 1,
+                    "source": "genbank-features",
+                    "contig": "AB123456",
+                    "legend": "CDS",
+                },
+            ]
+            found_expected_cds_features = []
+            for feature in json_data["cgview"]["features"]:
+                if feature["type"] == "CDS":
+                    print(feature)
+                    if feature in expected_cds_features:
+                        found_expected_cds_features.append(feature)
+            assert len(found_expected_cds_features) == len(
+                expected_cds_features
+            ), """The expected coding sequences are not present in the JSON file."""
+
+
+def test_genbank_to_cgview_on_reverse_strand() -> None:
+    """This uses the sample GenBank file from NCBI
+    (https://www.ncbi.nlm.nih.gov/Sitemap/samplerecord.html) to confirm the
+    manner in which features on the reverse strand are parsed. This is important
+    because the CGView JSON format requires that the start and stop positions of
+    features on the reverse strand be specified in the reverse order from the
+    way they are specified in the GenBank file."""
+    # Define path to an example genbank file (it is in the tests/data directory).
+    with resources.path("tests.data.genbank", "minimal_example_4.gbk") as genbank_path:
+        genbank_file = str(genbank_path)
+
+        # Define path for a temporary output directory.
+        with tempfile.TemporaryDirectory() as temp_output:
+            # Define path to temporary json file.
+            json_file = os.path.join(temp_output, "temp.json")
+
+            # Use the genbank_to_cgview_json function to convert the genbank file to a CGView JSON file.
+            genbank_to_cgview_json(genbank_file, json_file)
+
+            # Check that the JSON file was created.
+            assert os.path.exists(json_file)
+
+            # Check that the JSON file is not empty.
+            assert os.path.getsize(json_file) > 0
+
+            # Check that there are the correct number of CDS features in the JSON file.
+            with open(json_file) as json_fh:
+                json_data = json.load(json_fh)
+                num_cds_features = 0
+                for feature in json_data["cgview"]["features"]:
+                    if feature["type"] == "CDS":
+                        num_cds_features += 1
+                assert num_cds_features == 3
+
+            # Check that the start and stop positions of the CDS for the REV7 gene are correct.
+            rev7_gene_found = False
+            for feature in json_data["cgview"]["features"]:
+                if feature["type"] == "CDS":
+                    if feature["name"] == "REV7":
+                        rev7_gene_found = True
+                        assert feature["start"] == 4037
+                        assert feature["stop"] == 3300
+                        assert feature["strand"] == -1
+            assert rev7_gene_found
