220956964b6ac2de806acfb94d2445a2091da319
Lael Barlow <lael@ualberta.ca>
2025-07-11 16:49:43 -0600

    Remove mypy

--- noxfile.py
+++ noxfile.py
@@ -29,7 +29,6 @@ python_versions = ["3.13"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
-    "mypy",
     "pyright",
     "tests",
     "typeguard",
@@ -138,17 +137,6 @@ def precommit(session: Session) -> None:
     session.run("pre-commit", *args)
     if args and args[0] == "install":
         activate_virtualenv_in_precommit_hooks(session)
-
-
-@session(python=python_versions)
-def mypy(session: Session) -> None:
-    """Type-check using mypy."""
-    args = session.posargs or ["src", "tests", "docs/conf.py"]
-    session.install(".")
-    session.install("mypy", "pytest")
-    session.run("mypy", *args)
-    if not session.posargs:
-        session.run("mypy", f"--python-executable={sys.executable}", "noxfile.py")
 
 
 @session(python=python_versions)
